cffb84c248e17ef3038f63fea3a017fa71213a69
kuryart <kuryart1@gmail.com>
2026-04-13 13:23:54 -0300

    Added player face to face panel

--- nodes/battle/battle_player_ui.gd
+++ nodes/battle/battle_player_ui.gd
@@ -3,6 +3,9 @@ class_name BattlePlayerUI extends BattleBattlerUI
 var engine: BattleEngine
 var player: Player
 
+func _init() -> void:
+	focus_entered.connect(_on_focus_entered)
+
 func setup(_engine: BattleEngine, _player: Player):
 	engine = _engine
 	player = _player
@@ -10,3 +13,6 @@ func setup(_engine: BattleEngine, _player: Player):
 func _on_button_up():
 	super()
 	engine.battle_signals.player_selected.emit()
+
+func _on_focus_entered():
+	engine.battle_signals.player_changed.emit(player)
