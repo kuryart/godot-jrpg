f80758224b6074b62d32fa0f278a14084862b682
kuryart <kuryart1@gmail.com>
2026-04-13 17:10:35 -0300

    Now player's face appears when get damaged or attacking

--- resources/battle/phases/battle_phase_resolve_actions.gd
+++ resources/battle/phases/battle_phase_resolve_actions.gd
@@ -25,6 +25,7 @@ func resolve(engine: BattleEngine):
 		# --- Action ---
 		engine.battle_signals.change_player_face_emited.emit(action)
 		await action.resolve(engine)
+
 		
 		# --- Post-action ---
 	
